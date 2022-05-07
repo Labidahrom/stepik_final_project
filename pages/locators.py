f3f0d652f0357b71444113ee326a767a64899ed0
labidahrom@yandex.ru <labidahrom@yandex.ru>
2022-05-07 13:55:18 +0300

    Yet another commit

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,17 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    ITEM_NAME = (By.CSS_SELECTOR, "div#messages>div:nth-child(1) strong")
+    ITEM_PRICE = (By.CSS_SELECTOR, "div#messages>div:nth-child(3) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alertinner strong")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_cart_button.click()
+
+    def get_item_name_in_cart(self):
+        item_in_cart = self.browser.find_element(*ProductPageLocators.ITEM_NAME).text
+        return item_in_cart
+
+    def should_be_right_book(self, book_name):
+        assert self.get_item_name_in_cart() == book_name, "Incorrect book"
+
+    def get_item_price_in_cart(self):
+        item_prict_in_cart = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
+        return item_prict_in_cart
+
+    def should_be_right_price(self, book_price):
+        assert self.get_item_price_in_cart() == book_price, "Incorrect price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+        
+
