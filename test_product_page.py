34b4d09ff263527f7c85a7915add8c5814d96883
Labidahrom <labidahrom@yandex.ru>
2022-05-11 11:12:07 +0300

    final commit

--- test_product_page.py
+++ test_product_page.py
@@ -17,24 +17,25 @@ import random
 #                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.solve_quiz_and_get_code()
-    # page.should_be_right_book('Coders at Work')
-    # page.should_be_right_price('£19.99')
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.should_be_right_book('Coders at Work')
+    page.should_be_right_price('19,99 £')
 
-# @pytest.mark.xfail
-# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.solve_quiz_and_get_code()
-#     page.should_not_be_success_message()
-#
+@pytest.mark.need_review
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
 # def test_guest_cant_see_success_message(browser):
 #     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 #     page = ProductPage(browser, link)
@@ -56,13 +57,14 @@ import random
 #     page.open()
 #     page.should_be_login_link()
 
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
-#     login_page = LoginPage(browser, browser.current_url)
-#     login_page.should_be_login_page()
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 # def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/ru/"
@@ -83,21 +85,21 @@ class TestUserAddToBasketFromProductPage():
         page.register_new_user(generate_email, "Khfu23438z")
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
         page.open()
         page.add_to_cart()
-        # page.solve_quiz_and_get_code()
         page.should_be_right_book("Coders at Work")
         page.should_be_right_price('19,99 £')
 
+    @pytest.mark.xfail
     def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
         page.open()
         page.add_to_cart()
-        # page.solve_quiz_and_get_code()
         page.should_not_be_success_message()
 
 
